4a7a69daca991b9305fe7e0f86de2a52e440adf6
favtuts <favtuts@gmail.com>
2023-08-25 17:15:31 +0700

    create blank jobscheduling project

---
+++ spring-boot-scheduler/src/test/java/com/favtuts/sb/jobscheduling/JobschedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favtuts.sb.jobscheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobschedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
