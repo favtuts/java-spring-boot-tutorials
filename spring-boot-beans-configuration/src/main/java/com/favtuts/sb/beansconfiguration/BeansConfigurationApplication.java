d5f860b1447e23d3fc44b004b81a6de69d76edfa
favtuts <favtuts@gmail.com>
2023-08-30 23:23:34 +0700

    initial beans configuration project

---
+++ spring-boot-beans-configuration/src/main/java/com/favtuts/sb/beansconfiguration/BeansConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.favtuts.sb.conditionalbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeansConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConditionalbeansApplication.class, args);
+	}
+
+}
---
+++ spring-boot-beans-configuration/src/test/java/com/favtuts/sb/beansconfiguration/BeansConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favtuts.sb.conditionalbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeansConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
