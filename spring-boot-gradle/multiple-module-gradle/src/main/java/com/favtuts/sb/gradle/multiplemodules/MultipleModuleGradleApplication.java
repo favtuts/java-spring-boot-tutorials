6b9e28bcbbf79ee7d6575772db88cdab38294efb
favtuts <favtuts@gmail.com>
2023-09-06 11:53:18 +0700

    initial multiple modules spring boot project

---
+++ spring-boot-gradle/multiple-module-gradle/src/main/java/com/favtuts/sb/gradle/multiplemodules/MultipleModuleGradleApplication.java
@@ -0,0 +1,13 @@
+package com.favtuts.sb.gradle.multiplemodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleModuleGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleModuleGradleApplication.class, args);
+	}
+
+}
