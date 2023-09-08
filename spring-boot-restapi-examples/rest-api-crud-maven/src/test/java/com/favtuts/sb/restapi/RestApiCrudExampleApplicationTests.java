60f4b151e6723068b0de155b286d5d6da22de19b
favtuts <favtuts@gmail.com>
2023-09-08 11:24:34 +0700

    initial new spring boot restapi project with maven

---
+++ spring-boot-restapi-examples/rest-api-crud-maven/src/test/java/com/favtuts/sb/restapi/RestApiCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.favtuts.sb.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
