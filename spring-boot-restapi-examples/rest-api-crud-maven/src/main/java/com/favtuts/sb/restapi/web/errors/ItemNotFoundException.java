ea687768456de7ceb9b40a65e17029d9de1682ff
favtuts <favtuts@gmail.com>
2023-09-09 10:21:43 +0700

    configure the error-handling mechanism

---
+++ spring-boot-restapi-examples/rest-api-crud-maven/src/main/java/com/favtuts/sb/restapi/web/errors/ItemNotFoundException.java
@@ -0,0 +1,8 @@
+package com.favtuts.sb.restapi.web.errors;
+
+public class ItemNotFoundException extends RuntimeException {
+    private Long id;
+    public ItemNotFoundException(Long id) {
+        super("Could not find item " + id);
+    }
+}
