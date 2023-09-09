a66aefbf986e30e84cb7a10d445562801a1b7201
favtuts <favtuts@gmail.com>
2023-09-09 12:23:07 +0700

    provide more details in response for validation errors

--- spring-boot-restapi-examples/rest-api-crud-maven/src/main/java/com/favtuts/sb/restapi/web/errors/ApplicationExceptionHandler.java
+++ spring-boot-restapi-examples/rest-api-crud-maven/src/main/java/com/favtuts/sb/restapi/web/errors/ApplicationExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.favtuts.sb.restapi.web.errors;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
@@ -13,5 +19,16 @@ public class ApplicationExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String itemNotFoundHandler(ItemNotFoundException ex) {
         return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        List<String> details = new ArrayList<>();
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+            details.add(error.getDefaultMessage());
+        }
+        ErrorResponse error = new ErrorResponse("Validation Failed", details);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
---
+++ spring-boot-restapi-examples/rest-api-crud-maven/src/main/java/com/favtuts/sb/restapi/web/errors/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.favtuts.sb.restapi.web.errors;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@XmlRootElement(name = "error")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorResponse {
+    private String message;
+    private List<String> details;
+}
