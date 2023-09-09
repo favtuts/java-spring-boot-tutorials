26418a13b89c92c076e097f3b8e92947fb62f2e6
favtuts <favtuts@gmail.com>
2023-09-09 12:46:08 +0700

    add data initialization at application startup

--- spring-boot-restapi-examples/rest-api-crud-maven/src/main/java/com/favtuts/sb/restapi/web/ItemController.java
+++ spring-boot-restapi-examples/rest-api-crud-maven/src/main/java/com/favtuts/sb/restapi/web/ItemController.java
@@ -5,11 +5,13 @@ import com.favtuts.sb.restapi.repository.ItemRepository;
 import com.favtuts.sb.restapi.web.errors.ItemNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Validated
 public class ItemController {
 
     @Autowired
