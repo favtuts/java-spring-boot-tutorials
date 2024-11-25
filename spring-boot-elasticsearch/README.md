320e6199068a2964065d7d46d002a2da91f0b985
favtuts <favtuts@gmail.com>
2024-11-25 14:52:16 +0700

    init spring boot elasticsearch demo app

--- spring-boot-elasticsearch/README.md
+++ spring-boot-elasticsearch/README.md
@@ -2,3 +2,8 @@
 * https://tuts.heomi.net/getting-started-with-spring-data-elasticsearch/
 
 
+# Spring Boot Initializr
+
+![init-sb](./images/spring-boot-elasticsearch-demo-initializr.png)
+
+
---
+++ spring-boot-elasticsearch/sbelasticsearchdemo/src/main/java/com/favtuts/sbelasticsearchdemo/SbelasticsearchdemoApplication.java
@@ -0,0 +1,13 @@
+package com.favtuts.sbelasticsearchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbelasticsearchdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbelasticsearchdemoApplication.class, args);
+	}
+
+}
