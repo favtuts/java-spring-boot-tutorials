4c0f3e018ea86befac8646c093018ecd67e4aa8c
favtuts <favtuts@gmail.com>
2024-12-05 00:11:47 +0700

    hello message command line arguments

--- spring-boot-console-app/sbconsoleappdemo/src/main/java/com/favtuts/sbconsoleappdemo/SbconsoleappdemoApplication.java
+++ spring-boot-console-app/sbconsoleappdemo/src/main/java/com/favtuts/sbconsoleappdemo/SbconsoleappdemoApplication.java
@@ -1,13 +1,35 @@
 package com.favtuts.sbconsoleappdemo;
 
+import com.favtuts.sbconsoleappdemo.services.HelloService;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SbconsoleappdemoApplication {
+public class SbconsoleappdemoApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SbconsoleappdemoApplication.class, args);
+	private HelloService helloService;
+
+	public SbconsoleappdemoApplication(HelloService helloService) {
+		this.helloService = helloService;
 	}
 
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(SbconsoleappdemoApplication.class);
+		// disable spring banner
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// check if user passes any argument
+		if (args.length > 0) {
+			System.out.println(helloService.getMessage(args[0]));
+		} else {
+			// print the default message
+			System.out.println(helloService.getMessage());
+		}
+	}
 }
